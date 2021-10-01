80fbab7c25e39853558c59c5a16105c3a48d4980
Anurag Roy <anurag@2pirad.com>
2021-10-02 02:17:56 +0530

    Added get `/:id` route

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,29 @@
 import { Application, Router } from 'https://deno.land/x/oak@v9.0.1/mod.ts';
 import { oakCors } from 'https://deno.land/x/cors@v1.2.2/mod.ts';
-import { Pokemon } from './models/pokemon.ts';
+import { Pokemon, PokemonBase } from './models/pokemon.ts';
 
 const app = new Application();
 const router = new Router();
 
-router.get('/', async (context) => {
-    const pokemon: Pokemon[] = JSON.parse(await Deno.readTextFile('assets/data/index.json'));
-    context.response.body = pokemon;
-});
+router
+    .get('/', async (context) => {
+        const pokemon: PokemonBase[] = JSON.parse(
+            await Deno.readTextFile('assets/data/index.json')
+        );
+        context.response.body = pokemon;
+    })
+    .get('/:id', async (context) => {
+        const id = context.params.id;
+        try {
+            const pokemon: Pokemon[] = JSON.parse(
+                await Deno.readTextFile(`assets/data/${id}.json`)
+            );
+            context.response.body = pokemon;
+        } catch (error) {
+            console.log(error);
+            context.throw(404, 'Pokemon not found!');
+        }
+    });
 
 app.use(oakCors());
 app.use(router.routes());

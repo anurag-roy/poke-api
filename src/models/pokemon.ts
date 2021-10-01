7574990e4a3ccf921510dbae9294f6da118fcec7
Anurag Roy <anurag@2pirad.com>
2021-10-02 02:17:32 +0530

    Updated data with image URLs

--- src/models/pokemon.ts
+++ src/models/pokemon.ts
@@ -1,9 +1,13 @@
-export interface Pokemon {
+export interface PokemonBase {
     id: number;
     name: string;
     genus: string;
     description: string;
     types: string[];
+    imageUrl: string;
+}
+
+export interface Pokemon extends PokemonBase {
     abilities: {
         name: string;
         effect: string;
